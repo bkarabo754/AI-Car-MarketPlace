21ab4390180864b744a4956b1c19da8761a41d80
Bongani Masango <bkmasango26@gmail.com>
2025-04-22 08:48:33 +0200

    landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/embed',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-src 'self' https://car-marketplace-waitlist.created.app/",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
