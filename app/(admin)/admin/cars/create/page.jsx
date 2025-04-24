1476daf4ee209aaa6a64b0224d882b5984e1d614
Bongani Masango <bkmasango26@gmail.com>
2025-04-24 14:00:02 +0200

    Custom hook for fetching APIs

--- app/(admin)/admin/cars/create/page.jsx
+++ app/(admin)/admin/cars/create/page.jsx
@@ -1,3 +1,5 @@
+import AddCarForm from '../_components/add-car-form';
+
 export const metadata = {
   title: 'Add New Car | Vehiql Admin',
   description: 'Add a new car to the marketplace',
@@ -7,7 +9,7 @@ const AddCarPage = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Add New Car</h1>
-      {/* <AddCarForm /> */}
+      <AddCarForm />
     </div>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false, // default to true
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'joisflctofmpwncaomzy.supabase.co',
+      },
+    ],
+  },
+
   async headers() {
     return [
       {
